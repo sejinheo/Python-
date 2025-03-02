2aaabe38684ad951077202e485538a61605efde5
허세진 <heosejin222@gmail.com>
2025-03-02 22:19:50 +0900

    디코딩 파일 확인

--- python.py
+++ python.py
@@ -7,3 +7,8 @@ with open("output.wav", "wb") as f:
     f.write(audio_data)
 
 print("오디오 파일 저장 완료: output.wav")
+
+import os
+
+print("현재 작업 디렉토리:", os.getcwd())  # 현재 경로 출력
+print("파일 목록:", os.listdir())  # 현재 디렉토리에 있는 파일들 확인
